ab6d1ab8bb21dd9bb9f472078d722cbbb8e28001
Viswa Bonam <113131386+ViswaBonam@users.noreply.github.com>
2023-03-07 15:23:56 +0530

    Update #1 Danny's Diner.sql

--- #1 Danny's Diner.sql
+++ #1 Danny's Diner.sql
@@ -56,41 +56,41 @@ VALUES
   ----------------
   -- 1. What is the total amount each customer spent at the restaurant?
   
- SELECT s.customer_id, SUM(price) as Total_sales FROM sales s
- JOIN menu m on s.product_id=m.product_id
+ SELECT s.customer_id, SUM(price) AS Total_sales FROM sales s
+ JOIN menu m ON s.product_id=m.product_id
  Group by s.customer_id;
  
  -- 2. How many days has each customer visited the restaurant?
- SELECT customer_id, COUNT(distinct(order_date)) as Visits from sales 
+ SELECT customer_id, COUNT(distinct(order_date)) AS Visits FROM sales 
  GROUP BY customer_id;
  
 -- 3. What was the first item from the menu purchased by each customer?
  
- WITH ranked_orders as
+ WITH ranked_orders AS
 	(SELECT s.customer_id, s.order_date, m.product_name, 
-		DENSE_RANK() OVER(PARTITION BY s.customer_id ORDER BY s.order_date) as ranking
+		DENSE_RANK() OVER(PARTITION BY s.customer_id ORDER BY s.order_date) AS ranking
 	FROM Sales s
-	JOIN menu m on s.product_id=m.product_id)
+	JOIN menu m ON s.product_id=m.product_id)
 SELECT customer_id, product_name FROM ranked_orders
 WHERE ranking = 1
 GROUP BY customer_id, product_name;
   
 -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers? 
 
- SELECT m.product_name, COUNT(s.product_id) as most_purchased 
+ SELECT m.product_name, COUNT(s.product_id) AS most_purchased 
  FROM Sales s
- JOIN menu m on s.product_id=m.product_id
+ JOIN menu m ON s.product_id=m.product_id
  GROUP BY m.product_name
- ORDER BY most_purchased desc
+ ORDER BY most_purchased DESC
  LIMIT 1;
  
  -- 5. Which item was the most popular for each customer?
  
- WITH most_popular as
-	(SELECT s.customer_id, m.product_name, COUNT(s.product_id) as order_count,
-		DENSE_RANK() OVER(partition by s.customer_id order by COUNT(s.product_id) DESC) as ranking
+ WITH most_popular AS
+ 	(SELECT s.customer_id, m.product_name, COUNT(s.product_id) AS order_count,
+		DENSE_RANK() OVER(partition BY s.customer_id ORDER BY COUNT(s.product_id) DESC) AS ranking
 	FROM Sales s
-	JOIN menu m on s.product_id=m.product_id
+	JOIN menu m ON s.product_id=m.product_id
 	GROUP BY m.product_name, s.customer_id)
 SELECT customer_id, product_name, order_count FROM most_popular
 WHERE ranking = 1;
@@ -99,7 +99,7 @@ WHERE ranking = 1;
 
 WITH first_order_after_member AS
 	(SELECT s.customer_id, s.order_date, m.product_name,
-		DENSE_RANK() OVER(PARTITION BY s.customer_id ORDER BY s.order_date) as ranking
+		DENSE_RANK() OVER(PARTITION BY s.customer_id ORDER BY s.order_date) AS ranking
 	FROM sales s 
 	JOIN members mem ON s.customer_id=mem.customer_id
 	JOIN menu m ON s.product_id=m.product_id
@@ -111,7 +111,7 @@ WHERE ranking=1;
 
 WITH first_order_before_member AS
 	(SELECT s.customer_id, s.order_date, mem.join_date, m.product_name,
-		DENSE_RANK() OVER(PARTITION BY s.customer_id ORDER BY s.order_date desc) as ranking
+		DENSE_RANK() OVER(PARTITION BY s.customer_id ORDER BY s.order_date desc) AS ranking
 	FROM sales s 
 	JOIN members mem ON s.customer_id=mem.customer_id
 	JOIN menu m ON s.product_id=m.product_id
@@ -135,8 +135,8 @@ WITH product_price_points AS
 		CASE WHEN product_name="sushi" THEN price*20
 		ELSE price*10 END AS points
 	FROM menu)
-SELECT s.customer_id, SUM(p.points) as Total_points FROM product_price_points  p 
-JOIN sales s on p.product_id=s.product_id
+SELECT s.customer_id, SUM(p.points) AS Total_points FROM product_price_points  p 
+JOIN sales s ON p.product_id=s.product_id
 GROUP BY s.customer_id;
 
 -- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
@@ -145,17 +145,12 @@ GROUP BY s.customer_id;
 -- 3. SUM price and points
 
 WITH dates AS 
-(
-	SELECT 
-    *, 
-    DATE_ADD(join_date, INTERVAL 6 Day) AS valid_date, 
+	(SELECT *, 
+ 	DATE_ADD(join_date, INTERVAL 6 Day) AS valid_date, 
 	LAST_DAY('2021-01-31') AS last_date
-	FROM members AS m
-)
-
-SELECT  
-  d.customer_id, s.order_date, d.join_date, d.valid_date, d.last_date, m.product_name, m.price,
-SUM(CASE 
+	FROM members AS m)
+SELECT d.customer_id, s.order_date, d.join_date, d.valid_date, d.last_date, m.product_name, m.price,
+	SUM(CASE 
 		WHEN m.product_name = 'sushi' THEN 2 * 10 * m.price
 		WHEN s.order_date BETWEEN d.join_date AND d.valid_date THEN 2 * 10 * m.price
 		ELSE 10 * m.price END) AS points
